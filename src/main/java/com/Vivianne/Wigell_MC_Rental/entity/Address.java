dcb7d1a5f66ecd1d87dcbfc673e7deda56302c89
unknown <vivieri555@gmail.com>
2026-03-27 11:35:15 +0100

    up

---
+++ src/main/java/com/Vivianne/Wigell_MC_Rental/entity/Address.java
@@ -0,0 +1,85 @@
+package com.Vivianne.Wigell_MC_Rental.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String street;
+
+    @Column(nullable = false, length = 50)
+    private String city;
+
+    @Column(nullable = false, length = 50)
+    private String postalCode;
+
+    @OneToMany(
+            mappedBy = "address",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+
+    //@JsonIgnore
+    private List<Customer> customersList = new ArrayList<>();
+
+    protected Address() {}
+
+    public Address(String street, String city, String postalCode) {
+        this.street = street;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    @SuppressWarnings("unused")
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", postalCode=" + postalCode +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/Vivianne/Wigell_MC_Rental/entity/Customer.java
@@ -0,0 +1,72 @@
+package com.Vivianne.Wigell_MC_Rental.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "address_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_customer_address")
+    )
+    @Valid
+    private Address address;
+
+    @Column()
+    private String username;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName, Address address, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.username = username;
+    }
+    public Long getId() {
+        return id;
+    }
+    @SuppressWarnings("unused")
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
