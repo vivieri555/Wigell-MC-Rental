ca13a489f55e51919a6aacfc592b164c0f685165
unknown <vivieri555@gmail.com>
2026-03-27 10:28:45 +0100

    first commit

---
+++ src/main/java/com/Vivianne/Wigell_MC_Rental/WigellMcRentalApplication.java
@@ -0,0 +1,13 @@
+package com.Vivianne.Wigell_MC_Rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellMcRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellMcRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vivianne/Wigell_MC_Rental/WigellMcRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vivianne.Wigell_MC_Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellMcRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
